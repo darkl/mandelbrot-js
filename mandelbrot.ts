9ecad92a21f7f1545d76c3a495c7c8c4bebfcfca
Elad Zelingher <dark_leizar@walla.com>
2017-06-27 20:31:48 +0300

    Some refactor

--- mandelbrot.ts
+++ mandelbrot.ts
@@ -18,6 +18,198 @@
  *
  */
 
+interface IColorGenerator<TData> {
+    getColor(data: TData): number[];
+}
+
+abstract class BaseMandelbrotColorGenerator implements IColorGenerator<number[]> {
+    protected steps: number;
+
+    public getSteps() {
+        return this.steps;
+    }
+
+    constructor(steps: number) {
+        this.steps = steps;
+    }
+
+    getColor(data: number[]): number[] {
+        var [n, Tr, Ti] = data;
+
+        return this.colorPicker(n, Tr, Ti);
+    }
+
+    protected abstract colorPicker(n: number, Tr: number, Ti: number): number[];
+}
+
+
+
+
+class MandelbrotColorGenerators {
+// Some constants used with smoothColor
+    private static logBase = 1.0 / Math.log(2.0);
+    private static logHalfBase = Math.log(0.5) * 1.0 / Math.log(2.0);
+
+    public static smoothColor(steps: number, n: number, Tr: number, Ti: number): number {
+        /*
+         * Original smoothing equation is
+         *
+         * var v = 1 + n - Math.log(Math.log(Math.sqrt(Zr*Zr+Zi*Zi)))/Math.log(2.0);
+         *
+         * but can be simplified using some elementary logarithm rules to
+         */
+        return 5 + n - this.logHalfBase - Math.log(Math.log(Tr + Ti)) * this.logBase;
+    }
+}
+
+class HSV1 extends BaseMandelbrotColorGenerator {
+    protected colorPicker(n: number, Tr: number, Ti: number): number[] {
+        if (n === this.steps) // converged?
+            return interiorColor;
+
+        var v = MandelbrotColorGenerators.smoothColor(this.steps, n, Tr, Ti);
+        var c = hsv_to_rgb(360.0 * v / this.steps, 1.0, 1.0);
+        c.push(255); // alpha
+        return c;
+    }
+}
+
+class HSV2 extends BaseMandelbrotColorGenerator {
+    protected colorPicker(n: number, Tr: number, Ti: number): number[] {
+        if (n === this.steps) // converged?
+            return interiorColor;
+
+        var v = MandelbrotColorGenerators.smoothColor(this.steps, n, Tr, Ti);
+        var c = hsv_to_rgb(360.0 * v / this.steps, 1.0, 10.0 * v / this.steps);
+        c.push(255); // alpha
+        return c;
+    }
+}
+
+class HSV3 extends BaseMandelbrotColorGenerator {
+    protected colorPicker(n: number, Tr: number, Ti: number): number[] {
+        if (n === this.steps) // converged?
+            return interiorColor;
+
+        var v = MandelbrotColorGenerators.smoothColor(this.steps, n, Tr, Ti);
+        var c = hsv_to_rgb(360.0 * v / this.steps, 1.0, 10.0 * v / this.steps);
+
+        // swap red and blue
+        var t = c[0];
+        c[0] = c[2];
+        c[2] = t;
+
+        c.push(255); // alpha
+        return c;
+    }
+}
+
+class ColorGrayScale extends BaseMandelbrotColorGenerator {
+    protected colorPicker(n: number, Tr: number, Ti: number): number[] {
+        if (n === this.steps) // converged?
+            return interiorColor;
+
+        var v = MandelbrotColorGenerators.smoothColor(this.steps, n, Tr, Ti);
+        v = Math.floor(512.0 * v / this.steps);
+        if (v > 255) v = 255;
+        return [v, v, v, 255];
+    }
+}
+
+class ColorGrayScale2 extends ColorGrayScale {
+    protected colorPicker(n: number, Tr: number, Ti: number): number[] {
+        if (n === this.steps) { // converged?
+            var c = 255 - Math.floor(255.0 * Math.sqrt(Tr + Ti)) % 255;
+            if (c < 0) c = 0;
+            if (c > 255) c = 255;
+            return [c, c, c, 255];
+        }
+
+        return super.colorPicker(n, Tr, Ti);
+    }
+}
+
+class MandelbrotAlgorithm {
+    private _iterations: number;
+    private _escapeRadius: number;
+
+    constructor(radius: number, iterations: number) {
+        this._iterations = iterations;
+        this._escapeRadius = radius;
+    }
+
+    //  Main renderer equation.
+    //  
+    //  Returns number of iterations and values of Z_{n}^2 = Tr + Ti at the time
+    //  we either converged (n == iterations) or diverged.  We use these to
+    //  determined the color at the current pixel.
+    //  
+    //  The Mandelbrot set is rendered taking
+    //  
+    //      Z_{n+1} = Z_{n} + C
+    //  
+    //  with C = x + iy, based on the "look at" coordinates.
+    //  
+    //  The Julia set can be rendered by taking
+    //  
+    //      Z_{0} = C = x + iy
+    //      Z_{n+1} = Z_{n} + K
+    //  
+    //  for some arbitrary constant K.  The point C for Z_{0} must be the
+    //  current pixel we're rendering, but K could be based on the "look at"
+    //  coordinate, or by letting the user select a point on the screen.
+    public iterateEquation(cRealPart: number, cImaginaryPart: number) {
+        var zRealPart = 0;
+        var zImaginaryPart = 0;
+        var zRealSquared = 0;
+        var zImaginarySquared = 0;
+        var n = 0;
+
+        for (; n < this._iterations && (zRealSquared + zImaginarySquared) <= this._escapeRadius; ++n) {
+            zImaginaryPart = 2 * zRealPart * zImaginaryPart + cImaginaryPart;
+            zRealPart = zRealSquared - zImaginarySquared + cRealPart;
+            zRealSquared = zRealPart * zRealPart;
+            zImaginarySquared = zImaginaryPart * zImaginaryPart;
+        }
+
+        /*
+         * Four more iterations to decrease error term;
+         * see http://linas.org/art-gallery/escape/escape.html
+         */
+        for (var e = 0; e < 4; ++e) {
+            zImaginaryPart = 2 * zRealPart * zImaginaryPart + cImaginaryPart;
+            zRealPart = zRealSquared - zImaginarySquared + cRealPart;
+            zRealSquared = zRealPart * zRealPart;
+            zImaginarySquared = zImaginaryPart * zImaginaryPart;
+        }
+
+        return [n, zRealSquared, zImaginarySquared];
+    }
+
+    public static ColorGenerator = new class {
+        HSV1(steps: number) {
+            return new HSV1(steps);
+        }
+
+        HSV2(steps: number) {
+            return new HSV2(steps);
+        }
+
+        HSV3(steps: number) {
+            return new HSV3(steps);
+        }
+
+        Grayscale(steps: number) {
+            return new ColorGrayScale(steps);
+        }
+
+        Grayscale2(steps: number) {
+            return new ColorGrayScale2(steps);
+        };
+    }
+}
+
+
 /*
  * Global variables:
  */
@@ -60,68 +252,14 @@ function focusOnSubmit() {
     if (e) e.focus();
 }
 
-function getColorPicker(): (steps: number, n: number, Tr: number, Ti: number) => number[] {
-    var p = $<HTMLSelectElement>("colorScheme").value;
-    if (p === "pickColorHSV1") return pickColorHSV1;
-    if (p === "pickColorHSV2") return pickColorHSV2;
-    if (p === "pickColorHSV3") return pickColorHSV3;
-    if (p === "pickColorGrayscale2") return pickColorGrayscale2;
-    return pickColorGrayscale;
+function getColorPicker(): (steps) => IColorGenerator<number[]> {
+    var colorSchemeValue = $<HTMLSelectElement>("colorScheme").value;
+    return MandelbrotAlgorithm.ColorGenerator[colorSchemeValue];
 }
 
 function getSamples() {
     var i = parseInt($<HTMLInputElement>('superSamples').value, 10);
     return i <= 0 ? 1 : i;
-}
-
-/*
- * Main renderer equation.
- *
- * Returns number of iterations and values of Z_{n}^2 = Tr + Ti at the time
- * we either converged (n == iterations) or diverged.  We use these to
- * determined the color at the current pixel.
- *
- * The Mandelbrot set is rendered taking
- *
- *     Z_{n+1} = Z_{n} + C
- *
- * with C = x + iy, based on the "look at" coordinates.
- *
- * The Julia set can be rendered by taking
- *
- *     Z_{0} = C = x + iy
- *     Z_{n+1} = Z_{n} + K
- *
- * for some arbitrary constant K.  The point C for Z_{0} must be the
- * current pixel we're rendering, but K could be based on the "look at"
- * coordinate, or by letting the user select a point on the screen.
- */
-function iterateEquation(Cr: number, Ci: number, escapeRadius: number, iterations: number) {
-    var Zr = 0;
-    var Zi = 0;
-    var Tr = 0;
-    var Ti = 0;
-    var n = 0;
-
-    for (; n < iterations && (Tr + Ti) <= escapeRadius; ++n) {
-        Zi = 2 * Zr * Zi + Ci;
-        Zr = Tr - Ti + Cr;
-        Tr = Zr * Zr;
-        Ti = Zi * Zi;
-    }
-
-    /*
-     * Four more iterations to decrease error term;
-     * see http://linas.org/art-gallery/escape/escape.html
-     */
-    for (var e = 0; e < 4; ++e) {
-        Zi = 2 * Zr * Zi + Ci;
-        Zr = Tr - Ti + Cr;
-        Tr = Zr * Zr;
-        Ti = Zi * Zi;
-    }
-
-    return [n, Tr, Ti];
 }
 
 /*
@@ -286,7 +424,7 @@ function divRGB(v: number[], div: number) {
 /*
  * Render the Mandelbrot set
  */
-function draw(pickColor: (steps: number, n: number, Tr: number, Ti: number) => number[], superSamples: number) {
+function draw(generatorFactory: (steps:number) => IColorGenerator<number[]>, superSamples: number) {
     if (lookAt === null) lookAt = [-0.6, 0];
     if (zoom === null) zoom = [zoomStart, zoomStart];
 
@@ -312,6 +450,8 @@ function draw(pickColor: (steps: number, n: number, Tr: number, Ti: number) => n
 
     var steps = parseInt($<HTMLInputElement>('steps').value, 10);
 
+    var generator = generatorFactory(steps);
+
     if ($<HTMLInputElement>('autoIterations').checked) {
         var f = Math.sqrt(
             0.001 + 2.0 * Math.min(
@@ -333,6 +473,8 @@ function draw(pickColor: (steps: number, n: number, Tr: number, Ti: number) => n
     // Only enable one render at a time
     renderId += 1;
 
+    var fractal = new MandelbrotAlgorithm(escapeRadius, steps);
+
     function drawLineSuperSampled(Ci: number, off: number, Cr_init: number, Cr_step: number) {
         var Cr = Cr_init;
 
@@ -342,8 +484,8 @@ function draw(pickColor: (steps: number, n: number, Tr: number, Ti: number) => n
             for (var s = 0; s < superSamples; ++s) {
                 var rx = Math.random() * Cr_step;
                 var ry = Math.random() * Ci_step;
-                var p = iterateEquation(Cr - rx / 2, Ci - ry / 2, escapeRadius, steps);
-                color = addRGB(color, pickColor(steps, p[0], p[1], p[2]));
+                var p = fractal.iterateEquation(Cr - rx / 2, Ci - ry / 2);
+                color = addRGB(color, generator.getColor(p));
             }
 
             color = divRGB(color, superSamples);
@@ -359,8 +501,8 @@ function draw(pickColor: (steps: number, n: number, Tr: number, Ti: number) => n
         var Cr = Cr_init;
 
         for (var x = 0; x < canvas.width; ++x, Cr += Cr_step) {
-            var p = iterateEquation(Cr, Ci, escapeRadius, steps);
-            var color = pickColor(steps, p[0], p[1], p[2]);
+            var p = fractal.iterateEquation(Cr, Ci);
+            var color = generator.getColor(p);
             img.data[off++] = color[0];
             img.data[off++] = color[1];
             img.data[off++] = color[2];
@@ -450,78 +592,6 @@ function draw(pickColor: (steps: number, n: number, Tr: number, Ti: number) => n
     }
 
     render();
-}
-
-// Some constants used with smoothColor
-var logBase = 1.0 / Math.log(2.0);
-var logHalfBase = Math.log(0.5) * logBase;
-
-function smoothColor(steps: number, n: number, Tr: number, Ti: number): number {
-    /*
-     * Original smoothing equation is
-     *
-     * var v = 1 + n - Math.log(Math.log(Math.sqrt(Zr*Zr+Zi*Zi)))/Math.log(2.0);
-     *
-     * but can be simplified using some elementary logarithm rules to
-     */
-    return 5 + n - logHalfBase - Math.log(Math.log(Tr + Ti)) * logBase;
-}
-
-function pickColorHSV1(steps: number, n: number, Tr: number, Ti: number): number[] {
-    if (n === steps) // converged?
-        return interiorColor;
-
-    var v = smoothColor(steps, n, Tr, Ti);
-    var c = hsv_to_rgb(360.0 * v / steps, 1.0, 1.0);
-    c.push(255); // alpha
-    return c;
-}
-
-function pickColorHSV2(steps: number, n: number, Tr: number, Ti: number): number[] {
-    if (n === steps) // converged?
-        return interiorColor;
-
-    var v = smoothColor(steps, n, Tr, Ti);
-    var c = hsv_to_rgb(360.0 * v / steps, 1.0, 10.0 * v / steps);
-    c.push(255); // alpha
-    return c;
-}
-
-function pickColorHSV3(steps: number, n: number, Tr: number, Ti: number): number[] {
-    if (n === steps) // converged?
-        return interiorColor;
-
-    var v = smoothColor(steps, n, Tr, Ti);
-    var c = hsv_to_rgb(360.0 * v / steps, 1.0, 10.0 * v / steps);
-
-    // swap red and blue
-    var t = c[0];
-    c[0] = c[2];
-    c[2] = t;
-
-    c.push(255); // alpha
-    return c;
-}
-
-function pickColorGrayscale(steps: number, n: number, Tr: number, Ti: number): number[] {
-    if (n === steps) // converged?
-        return interiorColor;
-
-    var v = smoothColor(steps, n, Tr, Ti);
-    v = Math.floor(512.0 * v / steps);
-    if (v > 255) v = 255;
-    return [v, v, v, 255];
-}
-
-function pickColorGrayscale2(steps: number, n: number, Tr: number, Ti: number): number[] {
-    if (n === steps) { // converged?
-        var c = 255 - Math.floor(255.0 * Math.sqrt(Tr + Ti)) % 255;
-        if (c < 0) c = 0;
-        if (c > 255) c = 255;
-        return [c, c, c, 255];
-    }
-
-    return pickColorGrayscale(steps, n, Tr, Ti);
 }
 
 function main() {
